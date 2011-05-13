c45b1ef1558bf2c065043911655f7030dbe62a54
Alexander Orlov <alexander.orlov@loxal.net>
2011-05-13 12:14:57 +0200

    + counter capability; + post/store/datastore capability

--- src/main.go
+++ src/main.go
@@ -7,13 +7,137 @@ package main
 import (
 	"fmt"
 	"http"
+	"io"
+	"os"
+	"strconv"
+	"template"
+	"time"
+
+	"appengine"
+	"appengine/datastore"
+	"appengine/memcache"
+	"appengine/user"
 )
 
-func init() {
-	http.HandleFunc("/", handle)
+type Greeting struct {
+	Author  string
+	Content string
+	Date    datastore.Time
 }
 
-func handle(w http.ResponseWriter, r *http.Request) {
+//var mainPage = template.MustParse(`<html><body>
+//{.repeated section @}
+//{.section Author}<b>{@|html}</b>{.or}An anonymous person{.end}
+//wrote <blockquote>{Content|html}</blockquote>
+//{.end}
+//<form action="/store" method="post">
+//<div><textarea name="content" rows="3" cols="60">{blub}</textarea></div>
+//<div><input type="submit" value="Sign Guestbook"></div>
+//</form></body></html>`,
+//	nil)
+
+var mainPage = template.MustParseFile("template.html", nil)
+
+func hello(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	fmt.Fprint(w, "INIT...\n")
+	fmt.Fprint(w, "Hello, ...!\n")
+}
+
+func serveError(c appengine.Context, w http.ResponseWriter, err os.Error) {
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Header().Set("Content-Type", "text/plain")
+	io.WriteString(w, "Internal Server Error")
+	c.Logf("%v", err)
+}
+
+func serve404(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusNotFound)
+	w.Header().Set("Content-Type", "text/plain")
+	io.WriteString(w, "Not Found")
+}
+
+func count(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+
+	item, err := memcache.Get(c, r.URL.Path)
+	if err != nil && err != memcache.ErrCacheMiss {
+		serveError(c, w, err)
+		return
+	}
+	n := 0
+	if err == nil {
+		n, err = strconv.Atoi(string(item.Value))
+		if err != nil {
+			serveError(c, w, err)
+			return
+		}
+	}
+	n++
+	item = &memcache.Item{
+		Key:   r.URL.Path,
+		Value: []byte(strconv.Itoa(n)),
+	}
+	err = memcache.Set(c, item)
+	if err != nil {
+		serveError(c, w, err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/plain")
+	fmt.Fprintf(w, "%q has been visited %d times", r.URL.Path, n)
+}
+
+func handlePost(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" || r.URL.Path != postHandler {
+		serve404(w)
+		return
+	}
+	c := appengine.NewContext(r)
+	q := datastore.NewQuery("Greeting").Order("-Date").Limit(10)
+	var gg []*Greeting
+	_, err := q.GetAll(c, &gg)
+	if err != nil {
+		serveError(c, w, err)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html")
+	if err := mainPage.Execute(w, gg); err != nil {
+		c.Logf("%v", err)
+	}
+}
+
+func handleStore(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		serve404(w)
+		return
+	}
+	c := appengine.NewContext(r)
+	if err := r.ParseForm(); err != nil {
+		serveError(c, w, err)
+		return
+	}
+	g := &Greeting{
+		Content: r.FormValue("content"),
+		Date:    datastore.SecondsToTime(time.Seconds()),
+	}
+	if u := user.Current(c); u != nil {
+		g.Author = u.String()
+	}
+	if _, err := datastore.Put(c, datastore.NewIncompleteKey("Greeting"), g); err != nil {
+		serveError(c, w, err)
+		return
+	}
+	http.Redirect(w, r, postHandler, http.StatusFound)
+}
+
+var postHandler = "/post"
+var storeHandler string = "/store"
+var blub = "blubber"
+
+func init() {
+	http.HandleFunc("/", hello)
+	http.HandleFunc(postHandler, handlePost)
+	http.HandleFunc(storeHandler, handleStore)
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/count", count)
 }
