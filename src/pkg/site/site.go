0f6203be0c52192772f7b0cfe30cb32fb34d6534
Alexander Orlov <alexander.orlov@loxal.net>
2011-05-25 12:11:34 +0200

    - lang pkg

--- src/pkg/site/site.go
+++ src/pkg/site/site.go
@@ -8,11 +8,12 @@ import (
 	"fmt"
 	"http"
 	"io"
+	"io/ioutil"
+	"json"
 	"os"
 	"template"
 
 	"appengine"
-
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
@@ -33,13 +34,43 @@ func serve404(w http.ResponseWriter) {
 	io.WriteString(w, "Not Found")
 }
 
+func test() {
+	type Test struct {
+		Blub uint8
+	}
+	type Site struct {
+		Copyright, Title, TitleDesc string
+		Mail string
+		Test *Test
+	}
+	var j string = `{
+"Copyright": "Alexander Orlov. All rights reserved.", 
+"Title": "Sem — Sem Entity Manager | Loxal", 
+"TitleDesc": "Sem Entity Manager"
+}`
+	var s Site
+	fmt.Println(s.Title)
+	//fmt.Println(s.Copyright)
+result, _ := ioutil.ReadFile("./site_properties.json")
+err := json.Unmarshal([]byte(j), &s)
+err2 := json.Unmarshal([]byte(result), &s)
+fmt.Println(err)
+fmt.Println(err2)
+fmt.Println(string(result), "<<<<")
+fmt.Println(s.Title)
+fmt.Println(s.TitleDesc)
+fmt.Println(s.Copyright)
+fmt.Println(s.Mail)
+
+}
 
 func handleMain(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" || r.URL.Path != indexHandler {
 		serve404(w)
 		return
 	}
-
+//	test()
+fmt.Println(w.Header(), "<<<<<")
         w.Header().Set("Content-Type", "text/html")
         if err := mainPresenter.Execute(w, nil); err != nil {
         }
