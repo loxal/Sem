b8b8402c3ec1f6a13128c30566931ac9547a141e
Alexander Orlov <alexander.orlov@loxal.net>
2011-06-09 10:32:30 +0200

    fixing site switch bug

--- src/pkg/site/site.go
+++ src/pkg/site/site.go
@@ -5,50 +5,17 @@
 package site
 
 import (
-	"fmt"
 	"http"
-	"io"
-//	"io/ioutil"
-	"os"
 	"template"
 )
 
-func serveError(w http.ResponseWriter, err os.Error) {
-	w.WriteHeader(http.StatusInternalServerError)
-	w.Header().Set("Content-Type", plainTxtEnc)
-	io.WriteString(w, "Internal Server Error")
-	fmt.Fprintln(w, "%v", err)
-}
-
-func serve404(w http.ResponseWriter) {
-	w.WriteHeader(http.StatusNotFound)
-	w.Header().Set("Content-Type", plainTxtEnc)
-	io.WriteString(w, "Not Found")
-}
-
+// TODO going to be the catch-all handler?
 func handleMain(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		panic("TESTING 'panic'")
-		serve404(w)
-		return
-	}
 	w.Header().Set("Content-Type", "text/html")
-//	reloadMainPresenterTemplate() // auto-reload / refresh in dev mode
+	reloadMainPresenterTemplate() // auto-reload / refresh in dev mode
 	if err := mainPresenter.Execute(w, nil); err != nil {
 	}
 }
-
-//func handleProperties(w http.ResponseWriter, r *http.Request) {
-//	if r.Method != "GET" {
-//		panic("TESTING 'panic'")
-//		serve404(w)
-//		return
-//	}
-////	content, _ := ioutil.ReadFile("tmp/properties.json")
-//	content, _ := ioutil.ReadFile("static/client/site/properties.json") // DOESN'T WORK because the location is static
-//	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-//	fmt.Fprintln(w, string(content))
-//}
 
 func reloadMainPresenterTemplate() {
 	mainPresenter = template.New(nil)
@@ -59,15 +26,10 @@ func reloadMainPresenterTemplate() {
 }
 
 const indexHandler = "/"
-//const propertiesHandler = "/site.json"
 const plainTxtEnc = "text/plain; charset=utf-8"
-
-//const mainPresenterSite = "static/client/site/main.html"
 const mainPresenterSite = "pkg/site/main.html"
 var mainPresenter *template.Template
 
 func init() {
-	reloadMainPresenterTemplate()
 	http.HandleFunc(indexHandler, handleMain)
-//	http.HandleFunc(propertiesHandler, handleProperties)
 }
