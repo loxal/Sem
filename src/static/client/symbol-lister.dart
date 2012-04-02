d2628489c03d44b6486db48df16454ea1df47f54
Alexander Orlov <alexander.orlov@loxal.net>
2012-04-02 14:30:43 +0200

    + Hello World in Dart

---
+++ src/static/client/symbol-lister.dart
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2012 Alexander Orlov <alexander.orlov@loxal.net>. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+main() {
+  var colors = ['red', 'orange', 'green'];
+
+//  for (var i = 0; i < colors.length; i++) {
+  for (var i = 0; i < 10; i++) {
+    print(i);
+  }
+}
