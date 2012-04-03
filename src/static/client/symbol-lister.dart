e9c96061c12b47a744461af23078c052334917ef
Alexander Orlov <alexander.orlov@loxal.net>
2012-04-03 19:05:46 +0200

    + new samples

--- src/static/client/symbol-lister.dart
+++ src/static/client/symbol-lister.dart
@@ -4,15 +4,36 @@
  * license that can be found in the LICENSE file.
  */
 
-#library('sunflower');
+#library('test');
 
 #import('dart:html');
 
 main() {
-  var colors = ['radfdedd', 'oradnge', 'grdeen'];
-
-//  for (var i = 0; i < colors.length; i++) {
-  for (var i = 0; i < 3; i++) {
-    print(i);
+  for (int idx = 9985; idx < 10175; idx++) {
+//    print(new String.fromCharCodes([idx]));
   }
+
+    var msg = 'blub';
+  var loudify = (msg, testt1) => '!!! ${msg.toUpperCase()} ${testt1} !!!';
+  print (loudify("mdddob", 'test!!!!!??'));
+
+   var callbacks = [];
+    for (var i = 0; i < 2; i++) {
+      callbacks.add(() => print(i));
+    }
+    callbacks.forEach((c) => c());
+
+    fest(c) => print (c);
+
+    print((e) => print('33'));
+
+    print (fest(23));
+
+    var t = document.query('#symbol-from');
+    print(t);
+
+     var name = 'Bob';
+     if (name) {
+       print("You have a name!"); // Prints in JavaScript, not in Dart
+     }
 }
