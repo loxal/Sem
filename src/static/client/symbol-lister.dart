6392cb35b7fd5f34459698881047395b673f83dd
Alexander Orlov <alexander.orlov@loxal.net>
2012-04-10 14:30:45 +0200

    + full functionality in Dart

--- src/static/client/symbol-lister.dart
+++ src/static/client/symbol-lister.dart
@@ -8,23 +8,46 @@
 #import('dart:html');
 
 main() {
-    final int symbolFrom = Math.parseInt(document.query('#symbolFrom').value);
-    final int symbolTo = Math.parseInt(document.query('#symbolTo').value);
+    init();
+}
 
-  final Element list = document.query('#list');
-  int num = 0;
-  for (int idx = symbolFrom; idx < symbolTo; idx++) {
-     final symbol = new Element.html('<tr><td>' + num++ + '</td><td>' + new String.fromCharCodes([idx])+ '</td><td>' +
-      idx
-     + '</td></tr>');
+refreshSymbolList() {
+     final int symbolFrom = Math.parseInt(document.query('#symbolFrom').value);
+     final int symbolTo = Math.parseInt(document.query('#symbolTo').value);
 
-    list.nodes.add(symbol);
+   final Element list = document.query('#list');
+   list.nodes.clear();
+   int num = 0;
+   for (int idx = symbolFrom; idx < symbolTo; idx++) {
+      final symbol = new Element.html('<tr><td>' + num++ + '</td><td>' + new String.fromCharCodes([idx])+ '</td><td>' +
+       idx
+      + '</td></tr>');
+
+     list.nodes.add(symbol);
 
 
-    Element totalSymbols = document.query('#totalSymbols');
-    totalSymbols.innerHTML = (symbolTo - symbolFrom).toString();
+     final Element totalSymbols = document.query('#totalSymbols');
+     totalSymbols.innerHTML = (symbolTo - symbolFrom).toString();
 
-    Element decimalRange = document.query('#decimalRange');
-    decimalRange.innerHTML = symbolFrom.toString() + ' - ' + symbolTo.toString();
-  }
+     final Element decimalRange = document.query('#decimalRange');
+     decimalRange.innerHTML = symbolFrom.toString() + ' - ' + symbolTo.toString();
+   }
+}
+
+init() {
+    final Element click = document.query('#symbol-display');
+    click.on.click.add((e) => displaySymbol());
+
+    final Element refresh = document.query('#refresh');
+    refresh.on.click.add((e) => refreshSymbolList());
+
+    refreshSymbolList();
+}
+
+displaySymbol() {
+    final List<Element> a = document.queryAll('.symbol');
+    final Element symbolId = document.query('#symbolId');
+    for(Element e in a) {
+      e.innerHTML = '&#' + symbolId.value + ';';
+    }
 }
