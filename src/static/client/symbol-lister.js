562962904160791822402c4ed3ea1f5114b8345d
Alex <alexander.orlov@loxal.net>
2015-10-17 23:39:00 +0200

    cleanup project

--- src/static/client/symbol-lister.js
+++
@@ -1,6 +0,0 @@
-/*
- * Copyright 2012 Alexander Orlov <alexander.orlov@loxal.net>. All rights reserved.
- * Use of this source code is governed by a BSD-style
- * license that can be found in the LICENSE file.
- */
-
